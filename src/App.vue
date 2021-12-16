2d6b2125525c1ae1166a0a13e356b4e4bbcfe611
Иванов и Ко <32426965+kinoafisharu@users.noreply.github.com>
2021-12-16 16:26:36 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <Hello msg="Приветствуем наших клиентов!" />
+    <Links
+      msg="Подробнее о нас"
+      lnk1="https://www.youtube.com"
+      lnkName1="Наш канал"
+      lnk2="https://www.instagram.com"
+      lnkName2="Наш Инстаграмм"
+    />
+    <Links
+      msg="Наши партнеры"
+      lnk1="https://www.youtube.com"
+      lnkName1="Вася"
+      lnk2="https://www.instagram.com"
+      lnkName2="Петя"
+    />
+    <Links
+      msg="Сайт разработал"
+      lnk1="https://www.youtube.com"
+      lnkName1="Алексей"
+    />
+  </div>
+</template>
+
+<script>
+import Hello from "./components/Hello.vue";
+import Links from "./components/Links.vue";
+
+export default {
+  name: "App",
+  components: {
+    Hello,
+    Links,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Links.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="Links">
+    <h3>{{ msg }}</h3>
+    <ul>
+      <li>
+        <a href="lnk1" target="_blank" rel="noopener">{{ lnkName1 }} </a>
+      </li>
+      <li>
+        <a href="lnk2" target="_blank" rel="noopener">{{ lnkName2 }} </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Links",
+  props: {
+    lnk1: String,
+    lnkName1: String,
+    msg: String,
+    lnk1: String,
+    lnkName2: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
